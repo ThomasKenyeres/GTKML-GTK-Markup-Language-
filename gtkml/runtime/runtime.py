1f453e38cbef09be66aff3e2cfa02199f2618715
ThomasKenyeres <kenyerest97@gmail.com>
2017-11-16 15:14:28 +0100

    Runtime can access filesystem to run programs defined in XML files

--- gtkml/runtime/runtime.py
+++ gtkml/runtime/runtime.py
@@ -1,32 +1,41 @@
+from gtkml.runtime.execute.executor import Executor
 from gtkml.runtime.object_pool import ObjectPool
 from gtkml.parser.parser import DOMParser
 
 class GtkmlRuntime:
-    parser = DOMParser()
-
-    def __init__(self, start_file):
-        self.start_file = start_file
+    def __init__(self):
         self.object_pool = ObjectPool()
+        self.application = None
+        #self.assembler = ObjectAssembler()
+        self.parser = DOMParser()
 
-    @staticmethod
-    def _parse(xml):
-        return GtkmlRuntime.parser.parse(xml)
+    def _parse(self, xml):
+        return self.parser.parse(xml)
 
-    @staticmethod
-    def _read(path):
+    def _execute(self, root):
+        executor = Executor(root)
+        executor.execute()
+
+    def _read(self, path):
+        xml = ""
+        root = None
         with open(path, "r+") as xmlfile:
-            xml = ""
             for line in xmlfile.readlines():
                 xml += line.strip()
             #print(xml)
-            root = GtkmlRuntime._parse(xml)
+            root = self._parse(xml)
             print(root)
+        self._execute(root)
+
+    def run(self, start_file):
+        self._read(start_file)
 
 
-    def run(self):
-        self._read(self.start_file)
+
+import os
+RUNTIME = GtkmlRuntime()
+
 
 if __name__ == '__main__':
-    import os
-    runtime = GtkmlRuntime(os.getenv("HOME") + "/Asztal/gtkmltry.xml")
-    runtime.run()
+    RUNTIME.run(os.getenv("HOME") + "/Asztal/realgtk1.xml")
+    pass
