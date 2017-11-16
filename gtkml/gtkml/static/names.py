207a10d87ec67f0c466eb2de582feb2e1c097687
ThomasKenyeres <kenyerest97@gmail.com>
2017-11-16 15:06:42 +0100

    Some basic names/'keywords' added

--- gtkml/gtkml/static/names.py
+++ gtkml/gtkml/static/names.py
@@ -1,3 +1,44 @@
-NAMES = {
-    ""
+BUILTIN_NAMES = {
+    "application": "",
+    "window": "",
+    "title": "",
+    "body": "",
+    "python": "",
+
+    "box": "",
+    "grid": "",
+    "listbox": "",
+    "flowbox": "",
+    "actionbar": "",
+    "header": "",
+    "fixed": "",
+    "layout": "",
+
+    "label": "",
+    "img": "",
+    "spinner": "",
+    "progressbar": "",
+
+    "button": "",
+    "check": "",
+    "radio": "",
+    "toggle": "",
+    "linkbutton": "",
+
+    "entry": "",
+
+    "textview": "",
+    "editor": "",
+
+    "listview": "",
+
+    #SPECIAL
+    "table": "",
+    "th": "",
+    "tr": "",
+    "td": "",
+}
+
+USER_NAMES = {
+
 }
