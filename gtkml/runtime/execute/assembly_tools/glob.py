bac527cd0845c7f52cab7f3acdd0dd5d86e54b9b
ThomasKenyeres <kenyerest97@gmail.com>
2017-11-21 20:29:17 +0100

    numeric argument reading method

--- gtkml/runtime/execute/assembly_tools/glob.py
+++ gtkml/runtime/execute/assembly_tools/glob.py
@@ -15,3 +15,13 @@ def get_str_bool_attribute(tag, attr):
     if val == "True":
         return True
     return False
+
+def get_str_num_attribute(tag, attr):
+    val = get_attribute(tag, attr)
+    print(val)
+    try:
+        return float(val)
+    except ValueError:
+        return 0
+    except TypeError:
+        return 0
