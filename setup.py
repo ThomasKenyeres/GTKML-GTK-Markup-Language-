201898d16c729a4b7b402519fc70c6c6b5f1b740
ThomasKenyeres <kenyerest97@gmail.com>
2017-11-23 15:43:34 +0100

    Setup file added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='GTKML',
+    version='3.0.0',
+    packages=['gtkml', 'gtkml.gtkml', 'gtkml.gtkml.static', 'gtkml.gtkml.stdlib', 'gtkml.tools', 'gtkml.parser',
+              'gtkml.runtime', 'gtkml.runtime.execute', 'gtkml.runtime.execute.assembly_tools', 'gtkml.exception'],
+    url='',
+    license='',
+    author='thomas',
+    author_email='',
+    description='GTK+ application development tool'
+)
